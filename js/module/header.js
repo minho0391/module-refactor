9009bec074b24f53eb8f86ae14a640910e0edb6e
minho0391 <minh05@naver.com>
2026-05-28 17:49:16 +0900

    feat: create module header js

---
+++ js/module/header.js
@@ -0,0 +1,43 @@
+class Header {
+  constructor() {
+    this.header = document.querySelector("#header");
+  }
+
+  render() {
+    this.header.innerHTML = `
+      <div class="container d-flex justify-content-between align-items-center">
+        <h1 class="logo">
+          <a href="index.html">
+            <img src="images/logo.png" alt="acme logo" />
+            <span class="hidden">ACME</span>
+          </a>
+        </h1>
+
+        <nav>
+          <ul class="d-flex align-items-center">
+            <li>
+              <a href="index.html" class="text-nav">home</a>
+              /
+              <a href="about.html" class="text-nav">about</a>
+            </li>
+
+            <li>
+              <a href="signin.html" class="btn btn-secondary">
+                <span class="material-icons">lock</span>
+                sign in
+              </a>
+            </li>
+
+            <li>
+              <a href="signup.html" class="btn btn-primary">
+                sign up for free
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    `;
+  }
+}
+
+export default Header;
